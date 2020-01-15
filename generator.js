87f7dcb5e37ca2d35a35db9a5e7ad45bf5153352
yashpandit <yashpandit1995@gmail.com>
2020-01-16 00:04:46 +0530

    Add task for test dependency

--- generator.js
+++ generator.js
@@ -9,6 +9,7 @@ module.exports = app => {
 
   app.use(require('generate-project'));
   app.register('create-react-app', require('./lib/generators/create-react-app'));
+  app.register('custom-test', require('./lib/generators/custom-tests'));
 
   /**
    * Generate a `index.js` file to the current working directory. Learn how to [customize
